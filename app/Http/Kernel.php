952c252e59f1488e52bbd41d82463d14612d0e90
vishalwebtech123 <vishal@webtech-evolution.com>
2022-07-28 13:12:39 +0530

    cgange

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\isAdminCheck::class,
+        'role' => \App\Http\Middleware\Role::class,
     ];
 }
