421b198cf89124e7c091f6a3819b8add9482e912
pareshgit123 <mailto:paresh.greateideas@gmail.com>
2022-09-08 05:31:16 -0700

    change

--- resources/views/admin/role-permission/edit-form-permission.blade.php
+++
@@ -1,8 +0,0 @@
-{!! Form::open(['route' => ['permission.update',$permission->id], 'method' => 'patch' ]) !!}
-<div class="form-group">
-    <label class="form-label">permission title</label>
-    {{ Form::text('name', old('name',$permission->name), ['class' => 'form-control','id' => 'permission-title', 'placeholder' => 'Permission Title', 'required']) }}
-</div>
-<button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Save</button>
-<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-{{ Form::close() }}
