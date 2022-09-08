bad93609e69adf820c08f824b1a2c93740fa4c84
pareshgit123 <mailto:paresh.greateideas@gmail.com>
2022-09-08 03:49:54 -0700

    change

--- resources/views/admin/students/datatable.blade.php
+++
@@ -1,9 +0,0 @@
-{{--@extends('layouts.app')--}}
-{{--@push('third_party_stylesheets')--}}
-{{--    @include('layouts.datatables_css')--}}
-{{--@endpush--}}
-{{--{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered']) !!}--}}
-{{--@push('third_party_scripts')--}}
-{{--    @include('layouts.datatables_js')--}}
-{{--    {!! $dataTable->scripts() !!}--}}
-{{--@endpush--}}
